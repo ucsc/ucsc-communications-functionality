cd348243ce0537d4b5781da96ab94f49d9f4eae0
Jason Chafin <1000543+Herm71@users.noreply.github.com>
2025-07-18 11:49:03 -0700

    edits to plugin header

--- plugin.php
+++ plugin.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Plugin Name:       UC Santa Cruz Communications Custom Functionality plugin
+ * Plugin Name:       UCSC Communications Custom Functionality
  * Plugin URI:        https://example.com/blackbird-sandbox
- * Description:       Custom functionality for UCSC Communications.
+ * Description:       Custom functionality for UCSC Communications and Marketing Website.
  * Version:           0.1.0
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Author:            UC Santa Cruz Communications Team
+ * Author:            UC Santa Cruz
  * Author URI:        https://github.com/ucsc 
  * License:           GPL-3.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
