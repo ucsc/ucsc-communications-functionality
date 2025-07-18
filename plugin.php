ceeda58ab8c5caca070fac9a672af7b408d65d75
Jason Chafin <1000543+Herm71@users.noreply.github.com>
2025-07-18 11:25:27 -0700

    chore: :building_construction: Set up npm and composer, scaffold files

---
+++ plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       UC Santa Cruz Communications Custom Functionality plugin
+ * Plugin URI:        https://example.com/blackbird-sandbox
+ * Description:       Custom functionality for UCSC Communications.
+ * Version:           0.1.0
+ * Requires at least: 6.1
+ * Requires PHP:      7.0
+ * Author:            UC Santa Cruz Communications Team
+ * Author URI:        https://github.com/ucsc 
+ * License:           GPL-3.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Update URI:       https://github.com/ucsc/ucsc-communications-functionality-plugin/releases
+ * Requires Plugins:  advanced-custom-fields-pro
+ * Text Domain:       ucsccomms
+ *
+ * @package           ucsc-communications-functionality
+ */
