ab4fd5fe8bf6f4321f18a0a7179d61e139d2e3cd
Jason Chafin <1000543+Herm71@users.noreply.github.com>
2025-09-16 14:50:57 -0700

    chore(release): 1.0.2

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name:       UCSC Communications Custom Functionality
  * Plugin URI:        https://github.com/ucsc/ucsc-communications-functionality
  * Description:       Custom functionality for UCSC Communications and Marketing Website.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Requires at least: 6.1
  * Requires PHP:      7.0
  * Author:            UC Santa Cruz
